ae1b19bc0a20b579cc9e30fa0b412b3030cf5272
Bryce Stabenow <brycestabenow617@gmail.com>
2024-03-13 21:46:08 -0500

    first windowing setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main(){
-    println!("Hello world!");
+use iced::{Element, Settings, Sandbox};
+use iced::widget::text;
+
+fn main() -> iced::Result{
+    Editor::run(Settings::default())
+}
+
+struct Editor;
+
+#[derive(Debug)]
+enum Message {}
+
+impl Sandbox for Editor {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self
+    }
+
+    fn title(&self) -> String {
+        String::from("TxtGrind")
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {}
+    }
+
+    fn view(&self) -> Element<'_, Self::Message> {
+        text("Welcome to TxtGrind").into()
+    }
 }
